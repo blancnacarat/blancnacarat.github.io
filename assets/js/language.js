a2947c97a06c4f95d13826698e5fc467ebf8bd3a
Paul <nacarat.co@gmail.com>
2018-09-14 09:41:32 +1000

    language script
    http to https

--- assets/js/language.js
+++ assets/js/language.js
@@ -1,14 +1,18 @@
 $.ajax({ 
-    url: "http://grounded-will-216322.appspot.com", 
+    url: "https://grounded-will-216322.appspot.com", 
     dataType: 'jsonp', 
     success: function(headers) {
         language = headers['Accept-Language'];
         //nowDoSomethingWithIt(language);
         if (language = 'fr'){
+        	console.log("test");
+        	console.log(language);
         	setTimeout(function() {
   			window.location.href = "https://blancnacarat.github.io/fr/";
 			}, 3500);
         } else{
+        	console.log("test else");
+        	console.log(language);
         	setTimeout(function() {
   			window.location.href = "https://blancnacarat.github.io/en/";
 			}, 3500);
